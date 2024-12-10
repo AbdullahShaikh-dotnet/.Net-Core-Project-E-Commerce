864b61ad60f150def2b567bdacfced7a2d184495
abdullah034 <shaikhabdullah299@gmail.com>
2024-12-10 22:22:54 +0530

    Configured Entity Framwork. AppliationDbContext, Connection String, Sql server

--- E-Commerce/Models/Category.cs
+++ E-Commerce/Models/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace E_Commerce.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_Commerce.Models
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public int DisplayOrder { get; set; }
