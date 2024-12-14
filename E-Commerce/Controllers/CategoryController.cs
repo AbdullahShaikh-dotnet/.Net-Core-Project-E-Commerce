d8461dfd15d501e1aa5b5a613096786731161db4
abdullah034 <shaikhabdullah299@gmail.com>
2024-12-15 00:53:31 +0530

    Implement View Category List and Created UI for Create Category

--- E-Commerce/Controllers/CategoryController.cs
+++ E-Commerce/Controllers/CategoryController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Commerce.Data;
+using E_Commerce.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
+        {
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
