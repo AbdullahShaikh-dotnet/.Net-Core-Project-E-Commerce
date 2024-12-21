bb11425d05eaf6cb42679a90d98e44658d98af28
abdullah034 <shaikhabdullah299@gmail.com>
2024-12-21 15:11:50 +0530

    Implemented Category Repository Interface and Class

---
+++ ECom.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.DataAccess.Data;
+using ECom.DataAccess.Repository.IRepository;
+using ECom.Models;
+
+namespace ECom.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category CategoryObject)
+        {
+            _db.Categories.Update(CategoryObject);
+        }
+    }
+}
