d303c1bbdf768e7c5cc76fc3fc9f14df01f2090a
abdullah034 <shaikhabdullah299@gmail.com>
2024-12-21 14:58:56 +0530

    Implement Repository Pattern with IRepository Interface and Repository class

---
+++ ECom.DataAccess/Repository/Repository.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.DataAccess.Data;
+using ECom.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECom.DataAccess.Repository
+{
+    internal class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbset;
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            dbset = _db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbset.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbset;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbset;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbset.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbset.RemoveRange(entity);
+        }
+    }
+}
