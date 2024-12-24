4bff4b174407f118634d5f5cd143344caffd022f
abdullah034 <shaikhabdullah299@gmail.com>
2024-12-24 22:43:02 +0530

    Implemented Product Repository

---
+++ ECom.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using E_Commerce.DataAccess.Data;
+using ECom.DataAccess.Repository.IRepository;
+using ECom.Models;
+
+namespace ECom.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product ProductObject)
+        {
+            _db.Products.Update(ProductObject);
+        }
+    }
+}
