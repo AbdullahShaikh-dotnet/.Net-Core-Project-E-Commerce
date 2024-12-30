84e5746abd7d73b365afdf5170e83d1e8d4e962a
abdullah034 <shaikhabdullah299@gmail.com>
2024-12-30 22:56:29 +0530

    Handle Individual property during update in Product Repository

--- ECom.DataAccess/Repository/ProductRepository.cs
+++ ECom.DataAccess/Repository/ProductRepository.cs
@@ -20,6 +20,21 @@ namespace ECom.DataAccess.Repository
         public void Update(Product ProductObject)
         {
             _db.Products.Update(ProductObject);
+
+            Product? _product = _db.Products.FirstOrDefault(d => d.Id == ProductObject.Id);
+            if (_product == null) return;
+
+            _product.Title = ProductObject.Title;
+            _product.Author = ProductObject.Author;
+            _product.Category_ID = ProductObject.Category_ID;
+            _product.ISBN = ProductObject.ISBN;
+            _product.ListPrice = ProductObject.ListPrice;
+            _product.Price = ProductObject.Price;
+            _product.Price50 = ProductObject.Price50;
+            _product.Price100 = ProductObject.Price100;
+            _product.Description = ProductObject.Description;
+            if (ProductObject.ImageURL != null)
+                _product.ImageURL = ProductObject.ImageURL;
         }
     }
 }
