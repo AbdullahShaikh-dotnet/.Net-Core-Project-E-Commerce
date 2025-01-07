641fc0a5c8e2aaa3768545480e8255d2f0810571
abdullah034 <shaikhabdullah299@gmail.com>
2025-01-07 22:18:13 +0530

    Added Company Foreign Key Relation in Application User

--- ECom.Models/ApplicationUser.cs
+++ ECom.Models/ApplicationUser.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 
 namespace ECom.Models
 {
@@ -17,5 +19,11 @@ namespace ECom.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+
+        public int? CompanyID { get; set; }
+
+        [ForeignKey("CompanyID")]
+        [ValidateNever]
+        public Company Company { get; set; }
     }
 }
