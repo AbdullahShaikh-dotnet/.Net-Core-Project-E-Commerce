3a3c2004690b811bf8f7d1fa7c91fe14f0b00e06
abdullah034 <shaikhabdullah299@gmail.com>
2025-01-11 20:39:15 +0530

    Added Constant in Static Details in Utility

--- ECom.Utility/SD.cs
+++ ECom.Utility/SD.cs
@@ -8,9 +8,80 @@ namespace ECom.Utility
 {
     public static class SD
     {
+
+        /// <summary>
+        /// Role Status Admin 
+        /// </summary>
         public const string Role_Admin = "Admin";
+
+        /// <summary>
+        /// Role Status Employee 
+        /// </summary>
         public const string Role_Employee = "Employee";
+
+        /// <summary>
+        /// Role Status Customer 
+        /// </summary>
         public const string Role_Customer = "Customer";
+
+        /// <summary>
+        /// Role Status Company 
+        /// </summary>
         public const string Role_Company = "Company";
+
+
+
+
+        /// <summary>
+        /// Order Status Pending 
+        /// </summary>
+        public const string Status_Pending = "Pending";
+
+        /// <summary>
+        /// Order Status Approved 
+        /// </summary>
+        public const string Status_Approved = "Approved";
+
+        /// <summary>
+        /// Order Status In Processing 
+        /// </summary>
+        public const string Status_In_Process = "Processing";
+
+        /// <summary>
+        /// Order Status Shipped 
+        /// </summary>
+        public const string Status_Shipped = "Shipped";
+
+        /// <summary>
+        /// Order Status Cancelled 
+        /// </summary>
+        public const string Status_Cancelled = "Cancelled";
+
+        /// <summary>
+        /// Order Status Refunded 
+        /// </summary>
+        public const string Status_Refunded = "Refunded";
+
+
+
+        /// <summary>
+        /// Payment Status Pending 
+        /// </summary>
+        public const string Payment_Status_Pending = "Pending";
+
+        /// <summary>
+        /// Payment Status Approved 
+        /// </summary>
+        public const string Payment_Status_Approved = "Approved";
+
+        /// <summary>
+        /// Payment Status Approved for Delayed Payment 
+        /// </summary>
+        public const string Payment_Status_Delayed_Payment = "ApprovedForDelayedPayment";
+
+        /// <summary>
+        /// Payment Status Rejected 
+        /// </summary>
+        public const string Payment_Status_Rejected = "Rejected";
     }
 }
