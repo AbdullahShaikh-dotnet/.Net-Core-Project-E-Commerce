e480a94a7ebb7e822924db3183d618de773c68f3
abdullah034 <shaikhabdullah299@gmail.com>
2025-01-12 20:26:55 +0530

    Added Order View Model and Order Controller in admin Area

---
+++ E-Commerce/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using ECom.DataAccess.Repository;
+using ECom.DataAccess.Repository.IRepository;
+using ECom.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _UnitOfWork;
+        public OrderController(IUnitOfWork UnitOfWork)
+        {
+            _UnitOfWork = UnitOfWork;
+        }
+
+
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> orderHeaders = _UnitOfWork
+                .OrderHeaders
+                .GetAll(orderData => !orderData.IsDeleted, includePropertiesList: "ApplicationUsers")
+                .ToList();
+
+            return Json(new { data = orderHeaders });
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
