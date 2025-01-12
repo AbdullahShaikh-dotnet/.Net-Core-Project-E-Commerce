5e35a5f1a831f64374ebb2593b184da2d1fefd20
abdullah034 <shaikhabdullah299@gmail.com>
2025-01-12 20:56:36 +0530

    Implement Order List UI

--- E-Commerce/Areas/Admin/Controllers/OrderController.cs
+++ E-Commerce/Areas/Admin/Controllers/OrderController.cs
@@ -1,10 +1,14 @@
 ﻿using ECom.DataAccess.Repository;
 using ECom.DataAccess.Repository.IRepository;
 using ECom.Models;
+using ECom.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace E_Commerce.Areas.Admin.Controllers
 {
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
     public class OrderController : Controller
     {
         private readonly IUnitOfWork _UnitOfWork;
@@ -14,11 +18,12 @@ namespace E_Commerce.Areas.Admin.Controllers
         }
 
 
+        [HttpGet]
         public IActionResult GetAll()
         {
             List<OrderHeader> orderHeaders = _UnitOfWork
                 .OrderHeaders
-                .GetAll(orderData => !orderData.IsDeleted, includePropertiesList: "ApplicationUsers")
+                .GetAll(orderData => !orderData.IsDeleted, includePropertiesList: "_ApplicationUser")
                 .ToList();
 
             return Json(new { data = orderHeaders });
