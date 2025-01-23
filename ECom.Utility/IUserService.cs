fbf4aa273521dfae059dadc27688dc3d54438d8b
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-01-23 17:52:22 +0530

    Added User Cart Count Helper Methods

--- ECom.Utility/IUserService.cs
+++ ECom.Utility/IUserService.cs
@@ -1,9 +1,13 @@
 ﻿using System.Security.Claims;
+using ECom.Utility;
 using Microsoft.AspNetCore.Http;
 
 public interface IUserService
 {
     string GetUserId();
+    int? GetCartCount();
+    void SetCartCount(int CartCount);
+    void ClearCart();
 }
 
 public class UserService : IUserService
@@ -19,5 +23,21 @@ public class UserService : IUserService
     {
         var userClaims = _httpContextAccessor.HttpContext?.User.Identity as ClaimsIdentity;
         return userClaims?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+    }
+
+    public int? GetCartCount()
+    {
+        var CartCount = _httpContextAccessor.HttpContext.Session.GetInt32(SD.ShoppingCartSessionKey);
+        return CartCount;
+    }
+
+    public void SetCartCount(int CartCount)
+    {
+        _httpContextAccessor.HttpContext.Session.SetInt32(SD.ShoppingCartSessionKey, CartCount);
+    }
+
+    public void ClearCart()
+    {
+        _httpContextAccessor.HttpContext.Session.SetInt32(SD.ShoppingCartSessionKey, 0);
     }
 }
