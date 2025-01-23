7070ec5369155204959d832a787834e7654cbe66
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-01-23 17:53:01 +0530

    Created View Component

---
+++ E-Commerce/Areas/ViewComponents/ShoppingCartCountViewComponent.cs
@@ -0,0 +1,38 @@
+﻿using ECom.DataAccess.Repository.IRepository;
+using ECom.Utility;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Areas.ViewComponents
+{
+    public class ShoppingCartCountViewComponent : ViewComponent
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IUserService _userService;
+        public ShoppingCartCountViewComponent(IUnitOfWork unitOfWork, IUserService userService)
+        {
+            _unitOfWork = unitOfWork;
+            _userService = userService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var Userid = _userService.GetUserId();
+            if (Userid is not null)
+            {
+                if (_userService.GetCartCount() is null)
+                {
+                    int CartCount = _unitOfWork.ShoppingCarts
+                        .GetAll(cart => cart.ApplicationUserID == _userService.GetUserId() && !cart.IsDeleted)
+                        .Count() - 1;
+                    _userService.SetCartCount(CartCount);
+                }
+                return View(_userService.GetCartCount());
+            }
+            else
+            {
+                _userService.ClearCart();
+                return View(0);
+            }
+        }
+    }
+}
