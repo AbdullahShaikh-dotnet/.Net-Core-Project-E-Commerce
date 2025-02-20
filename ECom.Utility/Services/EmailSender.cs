64a359a0fe775ac659e288b83f1ec196ac6a7397
abdullah034 <shaikhabdullah299@gmail.com>
2025-02-21 01:14:00 +0530

    Implement Mailjet with Emailsender

--- ECom.Utility/Services/EmailSender.cs
+++ ECom.Utility/Services/EmailSender.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Identity.UI.Services;
+﻿using ECom.Utility.Interface;
+using ECom.Utility.Settings;
+using Mailjet.Client;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Linq;
 
 namespace ECom.Utility.Services
 {
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        private readonly IMailJetService _mailJetService;
+        public EmailSender(IMailJetService mailJetService)
         {
-            return Task.CompletedTask;
+            _mailJetService = mailJetService;
+        }
+
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //return Task.CompletedTask;
+            await _mailJetService.SendEmailAsync(email, subject, htmlMessage);
         }
     }
 }
