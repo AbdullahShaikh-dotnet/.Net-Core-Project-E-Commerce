1ae5a9ecfbeb5280632900b25c0b67af4b82efd0
abdullah034 <shaikhabdullah299@gmail.com>
2025-02-21 01:15:12 +0530

    UI Fix Bootstraps to Tailwind CSS

--- E-Commerce/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ E-Commerce/Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -116,7 +116,7 @@ namespace E_Commerce.Areas.Identity.Pages.Account.Manage
         {
             var activePage = viewContext.ViewData["ActivePage"] as string
                 ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
-            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "bg-blue-50 text-blue-600" : null;
         }
     }
 }
