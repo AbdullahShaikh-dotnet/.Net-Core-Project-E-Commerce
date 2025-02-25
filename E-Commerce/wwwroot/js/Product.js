cb0865d585b3e12b75db3d5b8984bf5929907b2e
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-02-25 18:38:19 +0530

    Datatable.js UI Fixed

--- E-Commerce/wwwroot/js/Product.js
+++ E-Commerce/wwwroot/js/Product.js
@@ -1,5 +1,9 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     LoadDataTables();
+    const searchInput = document.querySelector(".dt-search input");
+    if (searchInput) {
+        searchInput.placeholder = "Search";
+    }
 })
 
 let DT;
@@ -12,30 +16,6 @@ const LoadDataTables = function () {
     const DeleteURL = RootPath + tableElement.dataset?.deleteurl;
     DT = new DataTable(tableElement, {
         ajax: { url: GetURL },
-        layout: {
-            bottomStart: {
-                buttons: [
-                    {
-                        extend: 'csv',
-                        className: 'btn-sm btn-outline-primary border-1 rounded-1 mx-1', // Custom class
-                        filename: 'Product Report', // Custom filename
-                    },
-                    {
-                        extend: 'excel',
-                        className: 'btn-sm btn-outline-primary border-1 rounded-1 mx-1',
-                        filename: 'Product Report',
-                    },
-                    {
-                        extend: 'pdf',
-                        className: 'btn-sm btn-outline-primary border-1 rounded-1 mx-1',
-                        filename: 'Product Report',
-                        orientation: 'landscape', // Portrait or landscape
-                        pageSize: 'A4' // Page size
-                    }
-                ]
-            }
-        }
-        ,
         columns: [
             {
                 title: 'Sr. No.',
@@ -60,8 +40,8 @@ const LoadDataTables = function () {
                 title: 'Image',
                 data: 'imageURL',
                 render: function (d) {
-                    return d ? `<a href="${d}" target="_blank">
-                                    <img src=${d} width="auto" height="30" />
+                    return d ? `<a href="${d}" target="_blank" class="flex justify-center">
+                                    <img src=${d} width="50" height="30" />
                                 </a>` : '';
                 },
                 className: 'text-center',
@@ -73,13 +53,18 @@ const LoadDataTables = function () {
                 title: 'Actions',
                 render: function (d) {
                     return `
-                    	<div class="btn-group d-flex justify-content-center" role="group">
+                    	<div class="flex justify-between text-center">
 							<a class="mx-4 " href="${EditURL + d}">
-								<i class="bi bi-pencil-square fs-4" style="cursor: pointer"></i>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" /></svg>
 							</a>
 
-                            <i class="text-danger mx-4 bi bi-trash fs-4 btnDelete"
-                            data-url='${DeleteURL + d}' style="cursor: pointer"></i>
+
+                            <button class="text-red-500 btnDelete" data-url='${DeleteURL + d}'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>
+                            </button>
+
 						</div>
                     `;
                 },
@@ -90,6 +75,8 @@ const LoadDataTables = function () {
         select: true,
         processing: true,
         info: true,
+        scrollCollapse: true,
+        scrollY: '80vh',
         drawCallback: () => {
             // This function is triggered after every table redraw (e.g., after data is loaded)
             const deleteButtons = document.querySelectorAll('.btnDelete');
@@ -119,7 +106,7 @@ const DeleteConfirmation = function (url) {
             method: "POST",
         }).then(response => response.json())
             .then(data => {
-                toastr.success(data.message)
+                toast.success(data.message)
                 ReloadTable('.tblData');
             });
 
---
+++ E-Commerce/wwwroot/js/Toast.js
@@ -0,0 +1,78 @@
+﻿const toast = {
+    success: (message, timeout = 5000) => {
+        iziToast.success({
+            title: 'Success',
+            titleColor: '#064e3b', // Tailwind 'emerald-900'
+            message: message,
+            position: 'topCenter',
+            theme: 'light',
+            backgroundColor: '#d1fae5', // Tailwind 'emerald-100'
+            messageColor: '#065f46', // Tailwind 'emerald-700'
+            timeout: timeout * 1000, // Convert seconds to milliseconds
+            layout: 1,
+            close: true,
+            progressBarColor: '#10b981', // Tailwind 'emerald-500'
+            transitionIn: 'fadeInUp',
+            transitionOut: 'fadeOut',
+            drag: true
+        });
+    },
+
+    error: (message, timeout = 5000) => {
+        iziToast.error({
+            title: 'Error',
+            titleColor: '#7f1d1d', // Tailwind 'red-900'
+            message: message,
+            position: 'topCenter',
+            theme: 'light',
+            backgroundColor: '#fee2e2', // Tailwind 'red-100'
+            messageColor: '#991b1b', // Tailwind 'red-700'
+            timeout: timeout * 1000,
+            layout: 1,
+            close: true,
+            progressBarColor: '#ef4444', // Tailwind 'red-500'
+            transitionIn: 'fadeInUp',
+            transitionOut: 'fadeOut',
+            drag: true
+        });
+    },
+
+    warning: (message, timeout = 5000) => {
+        iziToast.warning({
+            title: 'Warning',
+            titleColor: '#78350f', // Tailwind 'amber-900'
+            message: message,
+            position: 'topCenter',
+            theme: 'light',
+            backgroundColor: '#fef3c7', // Tailwind 'amber-100'
+            messageColor: '#b45309', // Tailwind 'amber-700'
+            timeout: timeout * 1000,
+            layout: 1,
+            close: true,
+            progressBarColor: '#f59e0b', // Tailwind 'amber-500'
+            transitionIn: 'fadeInUp',
+            transitionOut: 'fadeOut',
+            drag: true
+        });
+    },
+
+    info: (message, timeout = 5000) => {
+        iziToast.info({
+            title: 'Info',
+            titleColor: '#1e3a8a', // Tailwind 'blue-900'
+            message: message,
+            position: 'topCenter',
+            theme: 'light',
+            backgroundColor: '#dbeafe', // Tailwind 'blue-100'
+            messageColor: '#1e40af', // Tailwind 'blue-700'
+            timeout: timeout * 1000,
+            layout: 1,
+            close: true,
+            progressBarColor: '#3b82f6', // Tailwind 'blue-500'
+            transitionIn: 'fadeInUp',
+            transitionOut: 'fadeOut',
+            drag: true
+        });
+    }
+
+};
