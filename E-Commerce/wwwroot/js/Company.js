f3d97a2389c24083774b8ffcf055a1a0e6a82b3a
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-02-26 12:10:56 +0530

    Toast Time Correction

--- E-Commerce/wwwroot/js/Company.js
+++ E-Commerce/wwwroot/js/Company.js
@@ -92,7 +92,7 @@ const DeleteConfirmation = function (url) {
             method: "POST",
         }).then(response => response.json())
             .then(data => {
-                toast.success(data.message);
+                toast.success(data.message,5);
                 ReloadTable('.tblData');
             });
 
