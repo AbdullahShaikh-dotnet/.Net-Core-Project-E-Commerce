8ad8ebcc01c92e178b80f2ffc14cde9fea57e57d
Abdullah Remort PC <shaikhabdullah299@gmail.com>
2025-03-07 16:54:53 +0530

    Added Filter View Model

---
+++ ECom.Models/ViewModels/ProductFilterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECom.Models.ViewModels
+{
+    public class ProductFilterViewModel
+    {
+        public List<string>? SelectedCategories { get; set; } = new();
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public string? SortBy { get; set; }
+
+        public IEnumerable<SelectListItem>? Categories { get; set; }
+        public IEnumerable<SelectListItem>? SortOptions { get; set; }
+        public IEnumerable<Product>? Products { get; set; }
+    }
+}
+
+
