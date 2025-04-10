12fac0b03fe5748a75b112dca8b30ae0017caa94
abdullah034 <shaikhabdullah299@gmail.com>
2025-04-10 22:33:12 +0530

    Fixed DeleteConfirmation Function Issue

--- E-Commerce/wwwroot/js/Product.js
+++ E-Commerce/wwwroot/js/Product.js
@@ -14,6 +14,7 @@ const LoadDataTables = function () {
     const GetURL = RootPath + tableElement.dataset?.geturl;
     const EditURL = RootPath + tableElement.dataset?.editurl;
     const DeleteURL = RootPath + tableElement.dataset?.deleteurl;
+
     DT = new DataTable(tableElement, {
         ajax: { url: GetURL },
         columns: [
@@ -60,7 +61,7 @@ const LoadDataTables = function () {
 							</a>
 
 
-                            <button class="text-red-500 btnDelete" data-url='${DeleteURL + d}'>
+                            <button class="text-red-500 btnDelete" onclick='DeleteConfirmation("${DeleteURL + d}")'>
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                                 <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>
                             </button>
@@ -76,17 +77,7 @@ const LoadDataTables = function () {
         processing: true,
         info: true,
         scrollCollapse: true,
-        scrollY: '50vh',
-        drawCallback: () => {
-            // This function is triggered after every table redraw (e.g., after data is loaded)
-            const deleteButtons = document.querySelectorAll('.btnDelete');
-            deleteButtons.forEach(button => {
-                button.addEventListener('click', (e) => {
-                    const URL = e.target.dataset.url
-                    DeleteConfirmation(URL);
-                });
-            });
-        }
+        scrollY: '50vh'
     });
 }
 
@@ -101,6 +92,8 @@ const DeleteConfirmation = function (url) {
         confirmButtonText: "Yes, delete it!"
     }).then((result) => {
         if (!result.isConfirmed) return;
+
+        debugger;
 
         fetch(url, {
             method: "POST",
