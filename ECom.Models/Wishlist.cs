cb2301ce06a416d5ea5508370eee72442855f16b
abdullah034 <shaikhabdullah299@gmail.com>
2025-04-21 00:28:39 +0530

    Added Wishlist Model

---
+++ ECom.Models/Wishlist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace ECom.Models
+{
+    public class Wishlist
+    {
+        public int ID { get; set; }
+
+
+
+        [ValidateNever]
+        [ForeignKey("ProductID")]
+        public Product product { get; set; }
+        public int ProductID { get; set; }
+
+
+
+        [ValidateNever]
+        [ForeignKey("ApplicationUserID")]
+        public ApplicationUser ApplicationUser { get; set; }
+        public string ApplicationUserID { get; set; }
+
+
+
+        [ValidateNever]
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+
+        [ValidateNever]
+        public bool IsDeleted { get; set; }
+
+
+        [ValidateNever]
+        public DateTime? DeletedAt { get; set; }
+    }
+}
