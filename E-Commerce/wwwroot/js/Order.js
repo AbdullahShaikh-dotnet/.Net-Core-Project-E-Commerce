52cd8de5f2f1eb3f379fc6776f73b3d62efb6a95
abdullah034 <shaikhabdullah299@gmail.com>
2025-04-22 22:24:29 +0530

    Added Loader Before Card DataLoading in Order MAnage View

--- E-Commerce/wwwroot/js/Order.js
+++ E-Commerce/wwwroot/js/Order.js
@@ -1,4 +1,5 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
+    showLoader_Cards();
     LoadDataTables();
     const searchInput = document.querySelector(".dt-search input");
     if (searchInput) {
@@ -105,4 +106,19 @@ const totalCountOrderCategory_Wise = () => {
         .then(data => {
             Object.keys(data).map(el => document.getElementById(el).innerHTML = data[el])
         });
+
+}
+
+const showLoader_Cards = function () {
+
+    const cardIdsObject =
+        ["inProgressTotalCount", "paymentPendingTotalCount", "completeTotalCount", "approvedTotalCount", "cancelledTotalCount", "delayedPaymentTotalCount"];
+
+    cardIdsObject.forEach(el => document.getElementById(el).innerHTML =
+        `<svg class="mr-3 -ml-1 size-5 animate-spin text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none"
+			  viewBox="0 0 24 24"><circle class="opacity-10" cx="12" cy="12" r="10"
+			  stroke="currentColor" stroke-width="4"></circle><path class="opacity-75"
+			  fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0
+			  12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+			  </path></svg>`);
 }
