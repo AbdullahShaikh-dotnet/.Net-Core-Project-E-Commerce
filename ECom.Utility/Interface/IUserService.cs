f4dde453611f51f2d961bcebb25069b2cb31698d
abdullah034 <shaikhabdullah299@gmail.com>
2025-04-27 21:08:11 +0530

    Added Wishlist Helper Methods Get, Set and Clear

--- ECom.Utility/Interface/IUserService.cs
+++ ECom.Utility/Interface/IUserService.cs
@@ -5,9 +5,19 @@ using System.Security.Claims;
 public interface IUserService
 {
     string GetUserId();
+
+
+    // Cart
     int? GetCartCount();
     void SetCartCount(int CartCount);
     void ClearCart();
+
+
+    // Wishlist
+    int? GetWishlistCount();
+    void SetWishlistCount(int WishlistCount);
+    void ClearWishlist();
+
 }
 
 public class UserService : IUserService
@@ -40,4 +50,23 @@ public class UserService : IUserService
     {
         _httpContextAccessor.HttpContext.Session.SetInt32(SD.ShoppingCartSessionKey, 0);
     }
+
+
+
+    public int? GetWishlistCount()
+    {
+        var WishlistCount = _httpContextAccessor.HttpContext.Session.GetInt32(SD.WishlistSessionKey);
+        return WishlistCount;
+    }
+
+    public void SetWishlistCount(int WishlistCount)
+    {
+        _httpContextAccessor.HttpContext.Session.SetInt32(SD.WishlistSessionKey, (int)WishlistCount);
+    }
+
+    public void ClearWishlist()
+    {
+        _httpContextAccessor.HttpContext.Session.SetInt32(SD.WishlistSessionKey, 0);
+    }
+
 }
